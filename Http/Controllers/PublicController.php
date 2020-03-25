d278b03f16272c9e36b220141201f599a06f6a73
fadriqueclickennet <48823970+fadriqueclickennet@users.noreply.github.com>
2020-03-25 10:01:56 +0100

    Repository creado

---
+++ Http/Controllers/PublicController.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Modules\Page\Http\Controllers;
+
+use Illuminate\Contracts\Foundation\Application;
+use Modules\Core\Http\Controllers\BasePublicController;
+use Modules\Menu\Repositories\MenuItemRepository;
+use Modules\Page\Entities\Page;
+use Modules\Page\Repositories\PageRepository;
+use Setting;
+
+class PublicController extends BasePublicController
+{
+    /**
+     * @var PageRepository
+     */
+    private $page;
+    /**
+     * @var Application
+     */
+    private $app;
+
+    private $disabledPage = false;
+
+    public function __construct(PageRepository $page, Application $app)
+    {
+        parent::__construct();
+        $this->page = $page;
+        $this->app = $app;
+    }
+
+    /**
+     * @param $slug
+     * @return \Illuminate\View\View
+     */
+    public function uri($slug)
+    {
+        $page = $this->findPageForSlug($slug);
+
+        // Si la pagina no existe, pagina personalizada 404
+        if (null === $page || $page->status === $this->disabledPage) {
+            //Si la web esta desactivada
+            if(Setting::get('core::active')==0){
+                if($this->auth->hasAccess('page.pages.index')){
+                    $page = $this->findPageForSlug('error-404');
+                    return view('errors.404', compact('page'));
+                }else{
+                    return view('layouts.construccion');
+                }
+            }else {
+                $page = $this->findPageForSlug('error-404');
+                return view('errors.404', compact('page'));
+            }
+        }
+
+        $currentTranslatedPage = $page->getTranslation(locale());
+        if ($slug !== $currentTranslatedPage->slug) {
+            return redirect()->to($currentTranslatedPage->locale . '/' . $currentTranslatedPage->slug, 301);
+        }
+
+        $template = $this->getTemplateForPage($page);
+
+        $this->addAlternateUrls($this->getAlternateMetaData($page));
+
+        //Si la web esta desactivada
+        if(Setting::get('core::active')==0){
+            if($this->auth->hasAccess('page.pages.index')){
+                return view($template, compact('page'));
+            }else{
+                return view('layouts.construccion');
+            }
+        }
+
+        return view($template, compact('page'));
+    }
+
+    /**
+     * @return \Illuminate\View\View
+     */
+    public function homepage()
+    {
+        $page = $this->page->findHomepage();
+
+        $this->throw404IfNotFound($page);
+
+        $template = $this->getTemplateForPage($page);
+
+        $this->addAlternateUrls($this->getAlternateMetaData($page));
+
+        //Si la web esta desactivada
+        if(Setting::get('core::active')==0){
+            if($this->auth->hasAccess('page.pages.index')){
+                return view($template, compact('page'));
+            }else{
+                return view('layouts.construccion');
+            }
+        }
+
+        
+        return view($template, compact('page'));
+    }
+
+    /**
+     * Find a page for the given slug.
+     * The slug can be a 'composed' slug via the Menu
+     * @param string $slug
+     * @return Page
+     */
+    private function findPageForSlug($slug)
+    {
+        $menuItem = app(MenuItemRepository::class)->findByUriInLanguage($slug, locale());
+
+        if ($menuItem) {
+            return $this->page->find($menuItem->page_id);
+        }
+
+        return $this->page->findBySlug($slug);
+    }
+
+    /**
+     * Return the template for the given page
+     * or the default template if none found
+     * @param $page
+     * @return string
+     */
+    private function getTemplateForPage($page)
+    {
+        return (view()->exists($page->template)) ? $page->template : 'default';
+    }
+
+    /**
+     * Throw a 404 error page if the given page is not found or draft
+     * @param $page
+     */
+    private function throw404IfNotFound($page)
+    {
+        if (null === $page || $page->status === $this->disabledPage) {
+            $this->app->abort('404');
+        }
+    }
+
+    /**
+     * Create a key=>value array for alternate links
+     *
+     * @param $page
+     *
+     * @return array
+     */
+    private function getAlternateMetaData($page)
+    {
+        $translations = $page->getTranslationsArray();
+
+        $alternate = [];
+        foreach ($translations as $locale => $data) {
+            $alternate[$locale] = $data['slug'];
+        }
+
+        return $alternate;
+    }
+}
